25c5f9cf4164538347de3ade68d2ec123fb30509
selim <salimmia3745@gmail.com>
2023-06-07 16:14:26 +0600

    Tests (73)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,15 +20,35 @@ const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
 
-// main is the main application function
+// main is the main function
 func main() {
-	// What I am going to put in the session
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
 	// change this to true when in production
 	app.InProduction = false
-	session = scs.New()
 
+	// set up the session
+	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
 	session.Cookie.SameSite = http.SameSiteLaxMode
@@ -36,35 +56,18 @@ func main() {
 
 	app.Session = session
 
-	// initializing Template cache
 	tc, err := render.CreateTemplateCache()
-
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
-	// store template cache to app config
 	app.TemplateCache = tc
-
 	app.UseCache = false
 
 	repo := handlers.NewRepo(&app)
-
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
-
-	// http.HandleFunc("/home", handlers.Repo.Home)
-
-	// http.HandleFunc("/about", handlers.Repo.About)
-
-	fmt.Printf("%v\n", fmt.Sprintf("Starting application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-	err = srv.ListenAndServe()
-
-	log.Fatal(err)
+	return nil
 }
